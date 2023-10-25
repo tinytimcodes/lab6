8b764740fce1a3ba80235d950d7cb1aa3cfe590b
timothychoy <tjc.choy@gmail.com>
2023-10-25 16:48:46 -0400

    tims code!

---
+++ main.py
@@ -0,0 +1,30 @@
+
+def encode(password):
+    new_pass = ""
+    for char in range(int(password)):
+        if char == 7:
+            new_pass += "0"
+        elif char == 8:
+            new_pass += "1"
+        elif char == 9:
+            new_pass += "2"
+        else:
+            new_pass += str(int(char) + 3)
+    return new_pass
+
+menu = True
+while menu == True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3.Quit")
+    user_input = input("Please enter an option: ")
+    if user_input == '1':
+        password = input("Please enter a password to encode: ")
+        encode(password)
+        print("Your password has been encoded and stored!")
+    elif user_input == "2":
+        print("this is where you work")
+    else:
+        menu = False
